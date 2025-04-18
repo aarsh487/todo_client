5721a2ea0647cafa682bc8e730cb1c004a033b6f
aarsh487 <aanandpatel487@gmail.com>
2025-04-19 01:19:49 +0530

    add url to fe

--- frontend/src/lib/axiosInstance.ts
+++ frontend/src/lib/axiosInstance.ts
@@ -4,7 +4,7 @@ const isProduction = import.meta.env.MODE === 'production';
 
 const axiosInstance = axios.create({
   baseURL: isProduction
-    ? 'https://clientto.netlify.app/api'
+    ? 'https://todo-client-byqj.onrender.com'
     : 'http://localhost:3000/api',
   withCredentials: true,
 });
